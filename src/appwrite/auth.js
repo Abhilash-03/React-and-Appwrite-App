96bc7ab69b9985819655e2c962a185c53ceee5ac
Abhilash <abhilashjha335@gmail.com>
2023-10-12 22:20:58 +0530

    build authentication services

---
+++ src/appwrite/auth.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-useless-catch */
+import config from "../config/config";
+import {Client, Account, ID} from "appwrite"
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        
+        this.account = new Account(this.client);
+    }
+
+   async createAccount({email, password, name}){
+    const userAccount = this.account.create(ID.unique(), email, password, name);
+    
+    if(userAccount){
+        // call another function (redirect login if sing-up successfull)
+        return this.login({email, password})
+    }
+    else{
+        return userAccount;
+    }
+    
+   }
+
+   async login({email, password}){
+    try {
+     return await this.account.createEmailSession(email, password);
+    } catch (error) {
+        throw error;
+    }
+   }
+
+   async getCurrentUser(){
+    try {
+        return this.account.get(); //to get the user
+    } catch (error) {
+        console.log("Appwrite service :: getCurrentUser :: error", error);
+    }
+    return null;
+   }
+
+   async logout(){
+    try{
+       return this.account.deleteSessions();
+    }catch(error){
+        console.log("Appwrite service :: logout :: error", error);
+    }
+   }
+}
+
+const authService = new AuthService();
+
+export default authService
