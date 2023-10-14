443993d4f8b60cf8e928f3a74c9e9bbd4df4bf0b
Abhilash <abhilashjha335@gmail.com>
2023-10-14 16:09:48 +0530

    add and update components

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import './App.css'
 import { useEffect, useState } from 'react';
 import authService from './appwrite/auth';
 import { login, logout } from './store/authSlice';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
+import { Header, Footer } from './components';
 
 function App() {
   const [isloading, setIsLoading] = useState(true);
@@ -26,7 +25,7 @@ function App() {
   return !isloading && <div className='min-h-screen flex flex-wrap content-between bg-orange-700 text-white text-3xl'>
     <div className='w-full block'>
       <Header />
-      <main>
+      <main className='bg-white'>
         {/* <Outlet /> */}
       </main>
       <Footer />
---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+function Logo({ width = "100px" }) {
+  return <div>logo</div>;
+}
+
+export default Logo;
